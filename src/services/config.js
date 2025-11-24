407c9a6814c9678feacb5d284c33848d8340c4d3
rrsc-dev <reis.rrsc@outlook.com>
2025-11-24 10:51:44 -0300

    exportação padrão de constante

---
+++ src/services/config.js
@@ -0,0 +1,7 @@
+// Variáveis e constantes também podem ser exportadas/importadas
+const devArea = {
+    version: "01",
+    production: true,
+}
+
+module.exports = devArea;
--- src/services/products.js
+++ src/services/products.js
@@ -3,7 +3,12 @@ async function getFullName(codeId, productName) {
     return codeId + "--" + productName;
 }
 
-// Define o que deve ser exportado desse arquivo
+async function getProductLabel(productName) {
+    console.log("Produto: " + productName);
+}
+
+// Define o que deve ser exportado desse arquivo, podem ser exportadas N funções
 module.exports = {
-    getFullName
+    getFullName,
+    getProductLabel
 }
