19997615948b186290ebbc5fddb5468603f04382
rrsc-dev <reis.rrsc@outlook.com>
2025-11-24 11:19:45 -0300

    conceito de hidden functions

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 // Faz a importação das funções em outros arquivos
-const p = require("./services/products"); // Recebe tudo que é exportável dentro do arquivo products
+const product = require("./services/products"); // Recebe tudo que é exportável dentro do arquivo products
 const config = require("./services/config");
 
 async function main() {
     // console.log('Executando');
     // console.log(p.getFullName("123", "Produto"))
-
-    console.log(config.production);
+    console.log(product.productType.version);
 }
 
 main();
--- src/services/products.js
+++ src/services/products.js
@@ -1,14 +1,30 @@
 // Funções que lidam com produtos
+const productType = {
+    version: "digital",
+    tax: "x1",
+}
+
+const apiURL = {
+    url: 'www.google.com',
+}
+
 async function getFullName(codeId, productName) {
-    return codeId + "--" + productName;
+    console.log(codeId + "--" + productName);
+    await doBreakLine();
 }
 
 async function getProductLabel(productName) {
     console.log("Produto: " + productName);
 }
 
+// Hidden function
+async function doBreakLine() {
+    console.log("\n");
+}
+
 // Define o que deve ser exportado desse arquivo, podem ser exportadas N funções
 module.exports = {
     getFullName,
-    getProductLabel
+    getProductLabel,
+    productType
 }
