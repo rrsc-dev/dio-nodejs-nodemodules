3d5e1288236354a549b3d448fa7a4e179d331644
rrsc-dev <reis.rrsc@outlook.com>
2025-11-24 11:29:53 -0300

    conceito de desestruturação

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // Faz a importação das funções em outros arquivos
-const product = require("./services/products"); // Recebe tudo que é exportável dentro do arquivo products
+ 
+// const product = require("./services/products"); // Recebe tudo que é exportável dentro do arquivo products
+
+const { getFullName, productType } = require("./services/products"); // Importa apenas o que está entre {} de dentro do móculo products
 const config = require("./services/config");
 const database = require("./services/database");
 
 async function main() {
     console.log(database.connectToDatabase("BD"));
+    getFullName("1", "Teclado");
 }
 
 main();
